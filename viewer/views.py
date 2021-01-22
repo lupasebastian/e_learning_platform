6d8cc36aa4518f40dfef27faac0e6356e1e16171
Sylwia Luu Quang <sylwialuuquang@gmail.com>
2021-01-22 22:59:56 +0100

    add base frontend and home page

--- viewer/views.py
+++ viewer/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+# Create your views here.
+
+
+class HomeView(TemplateView):
+    template_name = 'home.html'
