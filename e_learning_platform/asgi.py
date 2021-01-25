26a010decb87b4cedb5919f4243bcedec2ea37af
Sebastian <lupasebastian@gmail.com>
2021-01-25 16:02:33 +0100

    minor corrections in viewer/forms.py, some changes in e_learning_platform/settings.py, asgi.py, urls.py, working chat app, needs some adjustments as to where to be rendered in project and how to give it parameters (f.e. room name) so they don't have to be given explicitly by user

--- e_learning_platform/asgi.py
+++ e_learning_platform/asgi.py
@@ -8,9 +8,19 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from chat.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'e_learning_platform.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            websocket_urlpatterns
+        )
+    ),
+})
