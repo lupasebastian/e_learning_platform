204638092449a9ff1de0d70c9d6a07353893f61d
Rafał <rafalmik@gmail.com>
2021-01-25 21:32:42 +0100

    Accounts > Groups

---
+++ testsheet/forms.py
@@ -0,0 +1,45 @@
+from django.forms import ModelForm
+from .models import Test, TestQuestion, TestStudentAnswer, TestTeacherAnswer
+from viewer.models import Attachment
+
+
+class CreateTestForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    class Meta:
+        model = Test
+        fields = '__all__'
+
+class QuestionCreateForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    class Meta:
+        model = TestQuestion
+        fields = '__all__'
+
+class AnswerCreateForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    class Meta:
+        model = TestTeacherAnswer
+        fields = '__all__'
+
+class CreateAttachmentForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(self, *args, **kwargs)
+
+    class Meta:
+        model = Attachment
+        fields = '__all__'
+
+class FillTestForm(ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(self, *args, **kwargs)
+
+    class Meta:
+        model = TestStudentAnswer
+        fields = '__all__'
